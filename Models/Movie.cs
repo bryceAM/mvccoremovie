b5b3c1f212aec7ec5baf95771c002c87ee031fd2
bryceAM <bryce.malmberg@yahoo.com>
2025-05-13 22:14:44 -0500

    simple name change of MvcMovie to Movie class

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MvcMovie.Models;
 
-public class MvcMovie
+public class Movie
 {
     public int Id { get; set; }
     public string? Title { get; set; }

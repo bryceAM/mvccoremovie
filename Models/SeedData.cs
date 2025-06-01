a57e45b2582d2c4a449b9c42e3e84a998beace8f
bryceAM <bryce.malmberg@yahoo.com>
2025-06-01 00:53:25 -0500

    add rating for SeedData class

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -23,29 +23,33 @@ public static class SeedData
                 new Movie
                 {
                     Title = "When Harry Met Sally",
-                    ReleaseDate = DateTime.Parse("1989-2-12"),
+                    Rating = "R",
                     Genre = "Romantic Comedy",
+                    ReleaseDate = DateTime.Parse("1989-2-12"),
                     Price = 7.99M
                 },
                 new Movie
                 {
                     Title = "Ghostbusters ",
-                    ReleaseDate = DateTime.Parse("1984-3-13"),
+                    Rating = "PG-13",
                     Genre = "Comedy",
+                    ReleaseDate = DateTime.Parse("1984-3-13"),
                     Price = 8.99M
                 },
                 new Movie
                 {
                     Title = "Ghostbusters 2",
-                    ReleaseDate = DateTime.Parse("1986-2-23"),
+                    Rating = "PG-13",
                     Genre = "Comedy",
+                    ReleaseDate = DateTime.Parse("1986-2-23"),
                     Price = 9.99M
                 },
                 new Movie
                 {
                     Title = "Rio Bravo",
-                    ReleaseDate = DateTime.Parse("1959-4-15"),
+                    Rating = "PG-13",
                     Genre = "Western",
+                    ReleaseDate = DateTime.Parse("1959-4-15"),
                     Price = 3.99M
                 }
             );
